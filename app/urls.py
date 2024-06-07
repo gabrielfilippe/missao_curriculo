a648aca66957cb31e40dccf0701af0434b609531
gabrielfilippe <gabriel.filippe@alunos.ifsuldeminas.edu.br>
2024-06-07 11:45:31 -0300

    feat:adição de nova rota para vizualizar curriculo

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,8 @@ from django.urls import path
 urlpatterns = [
     path('', index, name='index'),
     path('pessoas/', pessoas, name='pessoas'),
+    path('curriculos/', curriculos, name='curriculos'),
     path('criar_curriculo/', criar_curriculo, name='criar_curriculo'),
     path('editar_curriculo/<int:id>/', editar_curriculo, name='editar_curriculo'),
+    path('excluir_curriculo/<int:id>/', excluir_curriculo, name='excluir_curriculo'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
