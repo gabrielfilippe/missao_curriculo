159599d8c3e0d60348bb0143669cc34ded6e67aa
gabrielfilippe <gabriel.filippe@alunos.ifsuldeminas.edu.br>
2024-06-11 15:28:07 -0300

    rota do filtro de curriculos

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('criar_curriculo/', criar_curriculo, name='criar_curriculo'),
     path('editar_curriculo/<int:id>/', editar_curriculo, name='editar_curriculo'),
     path('excluir_curriculo/<int:id>/', excluir_curriculo, name='excluir_curriculo'),
+    path('filtrar_curriculo/', filtrar_curriculo, name='filtrar_curriculo') #depois desse arquivo, vai para a views.py
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
